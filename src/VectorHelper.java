811a4137b3e527eaebc6cc0d9dca7456981359c2
johnlerouge <safsaf.abderraouf@gmail.com>
2017-10-15 23:13:01 +0100

    Add sumVectors and minAndMax medthods

--- src/VectorHelper.java
+++ src/VectorHelper.java
@@ -2,37 +2,6 @@ import java.util.Vector;
 
 public class VectorHelper{
 
-
-
-    public static void inverserVector(Vector<Integer> t){
-
-        int indexMax = t.size() - 1;
-        System.out.println(" SIZE : "+indexMax);
-        int indexMid = t.size() / 2 ;
-        System.out.println(" middle : "+indexMid);
-        int x;
-        for( int i=0;i< indexMid ; i++){
-
-           x = t.get(indexMax-i);
-            System.out.println(" loop : "+i+"\tt["+(indexMax-i)+"] : "+x);
-
-           t.set(indexMax-i,t.get(i));
-           t.set(i,x);
-
-        }
-
-    }
-
-    public static void afficherVector(Vector<Integer> t){
-
-        for(int i=0;i<t.size();i++){
-
-            System.out.println("index : "+i+"\tElement : "+t.get(i));
-
-        }
-
-    }
-
     public static void trierVector(Vector<Integer> t){
 
         boolean ordre = false;
@@ -44,14 +13,106 @@ public class VectorHelper{
             for(int i=0;i < indexMax - 1 ; i++)
             {
                 if(t.get(i) > t.get(i+1)){
-                   x = t.get(i);
-                   t.set(i,t.get(i+1));
-                   t.set(i+1,x);
-                   ordre = false;
+                    x = t.get(i);
+                    t.set(i,t.get(i+1));
+                    t.set(i+1,x);
+                    ordre = false;
                 }
             }
             indexMax--;
         }
 
+    }
+
+    /**
+     * This method is used to sum all the elements of two vectors and
+     * return a new vector that contains the result.
+     * <b>Note: The type of the two vectors is Integer and they
+     * must have the same size.</b>
+     * @param vect1 This is the first vector
+     * @param vect2 This is the second vector
+     * @return A Vector<Integer> that contains the result.
+     */
+    public static Vector<Integer> sumVectors(Vector<Integer> vect1, Vector<Integer> vect2) {
+
+        Vector<Integer> sumVector = new Vector<>();
+        try {
+
+            if (vect1.size() != vect2.size())
+                throw new DifferentVectorSizeException();
+
+            for (int i = 0; i < vect1.size(); i++) {
+
+                int sum = vect1.get(i) + vect2.get(i);
+                sumVector.add(i, sum);
+            }
+        }
+        catch (DifferentVectorSizeException e)  {
+
+            System.out.println("Error: the vector1 & vector2 sizes are not equals");
+        }
+
+        return sumVector;
+    }
+    /**
+     * <h1>VectorHelper</h1>
+     * <p>
+     * The VectorHelper class contains several static methods to simplify many Integer vectors'
+     * operations:
+     * Sorting, Addition of two vectors, Reverse elements, Get max and min,
+     * Apply a function to every element
+     * </p>
+     *
+     * @author Abderraouf Safsaf & Mohamed Nadji Souilamas
+     * @version 1.0
+     * @since 2017-10-14
+     */
+
+    public static void inverserVector(Vector<Integer> t){
+
+        int indexMax = t.size() - 1;
+        System.out.println(" SIZE : "+indexMax);
+        int indexMid = t.size() / 2 ;
+        System.out.println(" middle : "+indexMid);
+        int x;
+        for( int i=0;i< indexMid ; i++){
+
+            x = t.get(indexMax-i);
+            System.out.println(" loop : "+i+"\tt["+(indexMax-i)+"] : "+x);
+
+            t.set(indexMax-i,t.get(i));
+            t.set(i,x);
+
+        }
+
+    }
+
+    /**
+     * This method get the minimum and the maximum value from a vector
+     * @param vector A Vector<Integer> vector
+     * @return A Vector which the first element contains the Min value and the second element
+     * [1] contains the Max value
+     */
+    public static Vector<Integer> getMinAndMax(Vector<Integer> vector) {
+
+        Vector<Integer> resultVector = new Vector<>();
+
+        if (!vector.isEmpty())   {
+
+            int min = vector.get(0), max = vector.get(0);
+            for (int i = 0; i < vector.size(); i++) {
+
+                int v = vector.get(i);
+                if (v > max)
+                    max = v;
+                if (v < min)
+                    min = v;
+            }
+
+            resultVector.add(min);
+            resultVector.add(max);
+        }
+
+        return resultVector;
     }
 }
