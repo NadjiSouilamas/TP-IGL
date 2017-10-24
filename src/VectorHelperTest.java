7a7b1a5d2b4c8e0977382b2452bf42b621e86ace
Nadji <fm_souilamas@esi.dz>
2017-10-24 23:17:07 +0100

    Creating a conflict

--- src/VectorHelperTest.java
+++ src/VectorHelperTest.java
@@ -10,35 +10,34 @@ import static org.junit.Assert.*;
  */
 public class VectorHelperTest {
 
-    Vector<Integer> vect1, vect2;
+
+    Vector<Integer> v1 = new Vector<>(), v2 = new Vector<>();
 
     @Before
     public void setUp() throws Exception {
 
-        vect1 = new Vector<>();
-        vect1.add(14);
-        vect1.add(12);
-        vect1.add(30);
-        vect1.add(100);
-        vect1.add(21);
-        vect1.add(0);
+        v1.add(14);
+        v1.add(12);
+        v1.add(30);
+        v1.add(100);
+        v1.add(21);
+        v1.add(0);
 
-        vect2 = new Vector<>();
-        vect2.add(3);
-        vect2.add(2);
-        vect2.add(10);
-        vect2.add(100);
-        vect2.add(9);
-        vect2.add(333);
+        v2.add(3);
+        v2.add(2);
+        v2.add(10);
+        v2.add(100);
+        v2.add(9);
+        v2.add(333);
     }
 
     @Test
     public void trierVector() throws Exception  {
 
-        VectorHelper.trierVector(vect1);
-        for(int i = 0; i < vect1.size() - 1; i++){
+        VectorHelper.trierVector(v1);
+        for(int i = 0; i < v1.size() - 1; i++){
 
-            boolean isLowerThanSucc = vect1.get(i) < vect1.get(i+1);
+            boolean isLowerThanSucc = v1.get(i) < v1.get(i+1);
             assertTrue("Erreur dans le tri", isLowerThanSucc);
         }
     }
@@ -46,11 +45,11 @@ public class VectorHelperTest {
     @Test
     public void sumVectors() throws Exception {
 
-        Vector<Integer> sumVect = VectorHelper.sumVectors(vect1, vect2);
+        Vector<Integer> sumVect = VectorHelper.sumVectors(v1, v2);
 
-        for (int i = 0; i < vect1.size(); i++)  {
+        for (int i = 0; i < v1.size(); i++)  {
 
-            Integer expectedResult = vect1.get(i) + vect2.get(i);
+            Integer expectedResult = v1.get(i) + v2.get(i);
             Integer actualValue = sumVect.get(i);
 
             assertEquals(expectedResult, actualValue);
@@ -61,13 +60,13 @@ public class VectorHelperTest {
     public void inverserVector() throws Exception {
 
         Vector<Integer> initialVect = new Vector<Integer>();
-        initialVect = ( Vector<Integer> ) vect1.clone();
+        initialVect = ( Vector<Integer> ) v1.clone();
 
-        VectorHelper.inverserVector(vect1);
+        VectorHelper.inverserVector(v1);
 
-        for(int i = 0; i < vect1.size(); i++){
+        for(int i = 0; i < v1.size(); i++){
 
-            boolean areSwitched = vect1.get(i) == initialVect.get( initialVect.size() - 1 - i );
+            boolean areSwitched = v1.get(i) == initialVect.get( initialVect.size() - 1 - i );
             assertTrue("Erreur dans la procédure d'inversion", areSwitched);
         }
     }
@@ -75,26 +74,26 @@ public class VectorHelperTest {
     @Test
     public void getMinAndMax() throws Exception {
 
-        Vector<Integer> minMaxVect = VectorHelper.getMinAndMax(vect1);
+        Vector<Integer> minMaxVect = VectorHelper.getMinAndMax(v1);
 
         Integer min = minMaxVect.get(0),
                 max = minMaxVect.get(1);
 
-        for (int i = 0; i < vect1.size(); i++)  {
+        for (int i = 0; i < v1.size(); i++)  {
 
-            assertTrue(min <= vect1.get(i));
-            assertTrue(max >= vect1.get(i));
+            assertTrue(min <= v1.get(i));
+            assertTrue(max >= v1.get(i));
         }
     }
 
     @Test
     public void applyFormula() throws Exception {
 
-        Vector<Integer> initialVect = (Vector<Integer>) vect1.clone();
+        Vector<Integer> initialVect = (Vector<Integer>) v1.clone();
 
-        VectorHelper.applyFormula(vect1);
+        VectorHelper.applyFormula(v1);
 
-        for (int i = 0; i < vect1.size(); i++)    {
+        for (int i = 0; i < v1.size(); i++)    {
 
             Integer initialValue = initialVect.get(i);
             Integer expectedResult = initialValue;
